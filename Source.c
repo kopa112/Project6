41e9dfb34280069272f3f072469eba11a3866690
user <user@WIN-LNPL3JHKMJS>
2025-10-20 20:12:25 +0300

    Добавьте файлы проекта.

---
+++ Source.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+    int N;
+
+    printf("Введите число N (N < 1000): ");
+    scanf("%d", &N);
+
+    if (N < 1000) {
+
+        if (N == 5 || N % 10 == 5 || (N / 10) % 10 == 5 || (N / 100) % 10 == 5) {
+            printf("ДА, число %d содержит цифру 5\n", N);
+        }
+        else {
+            printf("НЕТ, число %d не содержит цифру 5\n", N);
+        }
+    }
+    else {
+        printf("Ошибка! Число должно быть меньше 1000\n");
+    }
+
+    return 0;
+}
